fad6c539d814560fdc409cce12ff44a86a4cef16
Ritika-Balodi <69412020+Ritika-Balodi@users.noreply.github.com>
2021-09-28 12:57:09 +0530

    Add files via upload

---
+++ Week0/week_5_2.c
@@ -0,0 +1,50 @@
+#include<stdio.h>
+int main()
+{
+    int i,j,k,key,n,t,temp,flag,c;
+    scanf("%d",&t);
+    for(k=0;k<t;k++)
+    {
+        scanf("%d",&n);
+        int a[n];
+        flag=0;
+        c=0;
+        for(i=0;i<n;i++)
+        {
+            scanf("%d",&a[i]);
+        }
+        scanf("%d",&key);
+        for(i=0;i<n-1;i++)
+        {
+            for(j=i+1;j<n;j++)
+            {
+                if(a[j]<a[i])
+                {
+                    temp=a[j];
+                    a[j]=a[i];
+                    a[i]=temp;
+                }
+            }
+        }
+        for(i=0;i<n;i++)
+        {
+            printf("%d ",a[i]);
+        }
+        for(i=0;i<n-1;i++)
+        {
+            for(j=i+1;j<n;j++)
+            {
+                if((a[i]+a[j])==key)
+                {
+                    if(c!=0)
+                        printf(",");
+                    printf("%d %d",a[i],a[j]);
+                    c++;
+                    flag=1;
+                }
+            }
+        }if(flag==0)
+            printf("No such pair exists.");
+    }
+
+}
